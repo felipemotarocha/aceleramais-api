a231444f61cc9f95d3843d7c77d21483b8a2173e
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-20 17:28:14 -0300

    feat: add RaceRepositoryAbstract

---
+++ src/repositories/race.repository.ts
@@ -0,0 +1,9 @@
+import { CreateRaceDto, GetAllRacesDto, UpdateRaceDto } from '../dtos/race.dtos'
+import Race from '../entities/race.entity'
+
+export interface RaceRepositoryAbstract {
+  create(createRaceDto: CreateRaceDto): Promise<Race>
+  getOne(id: string): Promise<Race>
+  getAll(getAllRacesDto: GetAllRacesDto): Promise<Race[]>
+  update(id: string, updateRaceDto: UpdateRaceDto): Promise<Race>
+}
