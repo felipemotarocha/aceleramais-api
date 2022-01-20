825b2df85428e3b8f79a960c1385c3c5ca8e7f58
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-20 16:09:10 -0300

    feat: add RaceServiceAbstract

---
+++ src/services/race/race.service.ts
@@ -0,0 +1,13 @@
+import {
+  CreateRaceDto,
+  GetAllRacesDto,
+  UpdateRaceDto
+} from '../../dtos/race.dtos'
+import Race from '../../entities/race.entity'
+
+export interface RaceServiceAbstract {
+  create(createRaceDto: CreateRaceDto): Promise<Race>
+  getOne(id: string): Promise<Race>
+  getAll(getAllRacesDto: GetAllRacesDto): Promise<Race[]>
+  update(updateRaceDto: UpdateRaceDto): Promise<Race>
+}
