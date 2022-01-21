138a5d26df82e554dc7212d8da82bdd97c5c9c43
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-21 18:48:58 -0300

    test: ensure RaceRepository returns a Race on getting one by id

--- src/repositories/race.repository.test.ts
+++ src/repositories/race.repository.test.ts
@@ -52,4 +52,43 @@ describe('Mongo Race Repository', () => {
 
     expect(createCollectionSpy).toHaveBeenCalledWith(dto)
   })
+
+  it('should get a Race by ID', async () => {
+    const sut = makeSut()
+
+    const dto = {
+      id: new Types.ObjectId() as any,
+      championship: new Types.ObjectId() as any,
+      classification: new Types.ObjectId() as any,
+      startDate: 'valid_start_date'
+    }
+
+    await RaceModel.create([{ _id: dto.id, ...dto }])
+
+    const result = await sut.getOne(dto.id)
+
+    expect(result.id).toBeTruthy()
+    expect(result.championship).toStrictEqual(dto.championship)
+    expect(result.classification).toStrictEqual(dto.classification)
+    expect(result.startDate).toBe(dto.startDate)
+  })
+
+  it('should call RaceModel findById method with correct values', async () => {
+    const sut = makeSut()
+
+    const createCollectionSpy = jest.spyOn(RaceModel, 'findById')
+
+    const dto = {
+      id: new Types.ObjectId() as any,
+      championship: new Types.ObjectId() as any,
+      classification: new Types.ObjectId() as any,
+      startDate: 'valid_start_date'
+    }
+
+    await RaceModel.create([{ _id: dto.id, ...dto }])
+
+    await sut.getOne(dto.id)
+
+    expect(createCollectionSpy).toHaveBeenCalledWith(dto.id)
+  })
 })
