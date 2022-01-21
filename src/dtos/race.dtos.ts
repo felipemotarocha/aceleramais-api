8f2ec88e068ff4c9a90bf7a52565952f1fb072cc
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-21 17:27:33 -0300

    feat: ensure RaceRepository returns a Race on creation success

--- src/dtos/race.dtos.ts
+++ src/dtos/race.dtos.ts
@@ -1,5 +1,5 @@
 export interface CreateRaceDto {
-  trackId: string
+  // trackId: string
   championship: string
   startDate: string
   classification: string
--- src/repositories/race.repository.ts
+++ src/repositories/race.repository.ts
@@ -1,5 +1,7 @@
 import { CreateRaceDto, GetAllRacesDto, UpdateRaceDto } from '../dtos/race.dtos'
 import Race from '../entities/race.entity'
+import MongooseHelper from '../helpers/mongoose.helpers'
+import _RaceModel from '../models/race.model'
 
 export interface RaceRepositoryAbstract {
   create(createRaceDto: CreateRaceDto): Promise<Race>
@@ -7,3 +9,31 @@ export interface RaceRepositoryAbstract {
   getAll(getAllRacesDto: GetAllRacesDto): Promise<Race[]>
   update(id: string, updateRaceDto: UpdateRaceDto): Promise<Race>
 }
+
+class MongoRaceRepository implements RaceRepositoryAbstract {
+  private readonly RaceModel: typeof _RaceModel
+
+  constructor(RaceModel: typeof _RaceModel) {
+    this.RaceModel = RaceModel
+  }
+
+  async create(createRaceDto: CreateRaceDto): Promise<Race> {
+    const race = await this.RaceModel.create(createRaceDto)
+
+    return MongooseHelper.map<Race>(race.toJSON())
+  }
+
+  getOne(id: string): Promise<Race> {
+    throw new Error('Method not implemented.')
+  }
+
+  getAll(getAllRacesDto: GetAllRacesDto): Promise<Race[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(id: string, updateRaceDto: UpdateRaceDto): Promise<Race> {
+    throw new Error('Method not implemented.')
+  }
+}
+
+export default MongoRaceRepository
