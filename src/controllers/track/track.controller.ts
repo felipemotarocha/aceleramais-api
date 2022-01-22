4125ae5cec20b556dac8a6f6a8073c3e8a5c6a02
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-21 22:54:39 -0300

    feat: ensure TrackController can get all the tracks

---
+++ src/controllers/track/track.controller.ts
@@ -0,0 +1,31 @@
+import { ServerError } from '../../errors/controllers.errors'
+import { badRequest, ok } from '../../helpers/controllers.helpers'
+import {
+  HttpRequest,
+  HttpResponse
+} from '../../protocols/controllers.protocols'
+import { TrackServiceAbstract } from '../../services/track/track.service'
+
+export interface TrackControllerAbstract {
+  getAll(httpRequest: HttpRequest): Promise<HttpResponse>
+}
+
+class TrackController implements TrackControllerAbstract {
+  private readonly trackService: TrackServiceAbstract
+
+  constructor(trackService: TrackServiceAbstract) {
+    this.trackService = trackService
+  }
+
+  async getAll(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const tracks = await this.trackService.getAll()
+
+      return ok(tracks)
+    } catch (_) {
+      return badRequest(new ServerError())
+    }
+  }
+}
+
+export default TrackController
