60d14d9095030afaa13ab5a1561586cab724d574
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-21 23:06:51 -0300

    test: ensure TrackController returns 400 if TrackService throws

--- src/controllers/track/track.controller.spec.ts
+++ src/controllers/track/track.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Types } from 'mongoose'
 
 import Track from '../../entities/track.entity'
+import { ServerError } from '../../errors/controllers.errors'
 import { TrackServiceAbstract } from '../../services/track/track.service'
 import TrackController, { TrackControllerAbstract } from './track.controller'
 
@@ -11,22 +12,45 @@ describe('Track Controller', () => {
     name: 'Autódromo José Carlos Pace'
   }
 
-  const makeSut = (): TrackControllerAbstract => {
+  interface SutTypes {
+    sut: TrackControllerAbstract
+    trackServiceStub: TrackServiceAbstract
+  }
+
+  const makeSut = (): SutTypes => {
     class TrackServiceStub implements TrackServiceAbstract {
       async getAll(): Promise<Track[]> {
         return [validTrack]
       }
     }
 
-    return new TrackController(new TrackServiceStub())
+    const trackServiceStub = new TrackServiceStub()
+    const sut = new TrackController(trackServiceStub)
+
+    return { sut, trackServiceStub }
   }
 
   it('should get all the Tracks', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const result = await sut.getAll({})
 
     expect(result.statusCode).toBe(200)
     expect(result.body).toStrictEqual([validTrack])
+  })
+
+  it('should return 400 if ProductService throws', async () => {
+    const { sut, trackServiceStub } = makeSut()
+
+    jest
+      .spyOn(trackServiceStub, 'getAll')
+      .mockReturnValueOnce(
+        new Promise((_resolve, reject) => reject(new Error()))
+      )
+
+    const result = await sut.getAll({})
+
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toStrictEqual(new ServerError())
   })
 })
