2c2e2715fb292c913c6c7131238f5098e3a2c9df
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-21 22:48:53 -0300

    test: ensure TrackService can get all the tracks

---
+++ src/services/track/track.service.spec.ts
@@ -0,0 +1,31 @@
+import { Types } from 'mongoose'
+
+import Track from '../../entities/track.entity'
+import { TrackRepositoryAbstract } from '../../repositories/track/track.repository'
+import TrackService, { TrackServiceAbstract } from './track.service'
+
+describe('Track Service', () => {
+  const validTrack = {
+    id: new Types.ObjectId() as any,
+    countryCode: 'BR',
+    name: 'Autódromo José Carlos Pace'
+  }
+
+  const makeSut = (): TrackServiceAbstract => {
+    class TrackRepositoryStub implements TrackRepositoryAbstract {
+      async getAll(): Promise<Track[]> {
+        return [validTrack]
+      }
+    }
+
+    return new TrackService(new TrackRepositoryStub())
+  }
+
+  it('should get all the Tracks', async () => {
+    const sut = makeSut()
+
+    const result = await sut.getAll()
+
+    expect(result).toStrictEqual([validTrack])
+  })
+})
