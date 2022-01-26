5bfd12596295f8cceb324dd99a06c7f4dd0f4f57
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-26 14:33:14 -0300

    feat: ensure ScoringSystemService can create  a ScoringSystem

---
+++ src/services/scoring-system/scoring-system.service.ts
@@ -0,0 +1,45 @@
+import {
+  CreateScoringSystemDto,
+  UpdateScoringSystemDto
+} from '../../dtos/scoring-system.dtos'
+import ScoringSystem from '../../entities/scoring-system.entity'
+import { ScoringSystemRepositoryAbstract } from '../../repositories/scoring-system/scoring-system.repository'
+
+export interface ScoringSystemServiceAbstract {
+  create(createScoringSystemDto: CreateScoringSystemDto): Promise<ScoringSystem>
+  getOne({ championship }: { championship: string }): Promise<ScoringSystem>
+  update(
+    id: string,
+    updateScoringSystemDto: UpdateScoringSystemDto
+  ): Promise<ScoringSystem>
+  delete(id: string): Promise<ScoringSystem>
+}
+
+export class ScoringSystemService implements ScoringSystemServiceAbstract {
+  private readonly scoringSystemRepository: ScoringSystemRepositoryAbstract
+
+  constructor(scoringSystemRepository: ScoringSystemRepositoryAbstract) {
+    this.scoringSystemRepository = scoringSystemRepository
+  }
+
+  async create(
+    createScoringSystemDto: CreateScoringSystemDto
+  ): Promise<ScoringSystem> {
+    return await this.scoringSystemRepository.create(createScoringSystemDto)
+  }
+
+  getOne({ championship }: { championship: string }): Promise<ScoringSystem> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(
+    id: string,
+    updateScoringSystemDto: UpdateScoringSystemDto
+  ): Promise<ScoringSystem> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(id: string): Promise<ScoringSystem> {
+    throw new Error('Method not implemented.')
+  }
+}
