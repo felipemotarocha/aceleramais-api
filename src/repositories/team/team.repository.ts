08faa2671a230cafc6f04a4e1e7b577030f35d2a
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-25 23:06:48 -0300

    feat: ensure TeamRepository can create a Team

---
+++ src/repositories/team/team.repository.ts
@@ -0,0 +1,37 @@
+import { CreateTeamDto, UpdateTeamDto } from '../../dtos/team.dtos'
+import Team from '../../entities/team.entity'
+import MongooseHelper from '../../helpers/mongoose.helpers'
+import _TeamModel from '../../models/team.model'
+
+export interface TeamRepositoryAbstract {
+  create(createTeamDto: CreateTeamDto): Promise<Team>
+  getAll(championship: string): Promise<Team>
+  update(updateTeamDto: UpdateTeamDto): Promise<Team>
+  delete(id: string): Promise<Team>
+}
+
+export class MongoTeamRepository implements TeamRepositoryAbstract {
+  private readonly TeamModel: typeof _TeamModel
+
+  constructor(TeamModel: typeof _TeamModel) {
+    this.TeamModel = TeamModel
+  }
+
+  async create(createTeamDto: CreateTeamDto): Promise<Team> {
+    const team = await this.TeamModel.create(createTeamDto)
+
+    return MongooseHelper.map<Team>(team.toJSON())
+  }
+
+  getAll(championship: string): Promise<Team> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(updateTeamDto: UpdateTeamDto): Promise<Team> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(id: string): Promise<Team> {
+    throw new Error('Method not implemented.')
+  }
+}
