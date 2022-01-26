ea200a7a248150bde15242bb028a07134a4db600
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-26 10:50:57 -0300

    test: ensure TeamController returns 400 if required fields are not provided on creation

--- src/controllers/team/team.controller.spec.ts
+++ src/controllers/team/team.controller.spec.ts
@@ -2,6 +2,7 @@ import { Types } from 'mongoose'
 
 import { CreateTeamDto, UpdateTeamDto } from '../../dtos/team.dtos'
 import Team from '../../entities/team.entity'
+import { MissingParamError } from '../../errors/controllers.errors'
 import { TeamServiceAbstract } from '../../services/team/team.service'
 import { TeamController, TeamControllerAbstract } from './team.controller'
 
@@ -52,7 +53,7 @@ describe('Team Controller', () => {
 
     const dto = {
       championship: 'valid_championship',
-      naem: 'valid_name',
+      name: 'valid_name',
       color: 'valid_color'
     }
 
@@ -71,12 +72,40 @@ describe('Team Controller', () => {
 
     const dto = {
       championship: 'valid_championship',
-      naem: 'valid_name',
+      name: 'valid_name',
       color: 'valid_color'
     }
 
     await sut.create({ body: dto })
 
     expect(createTeamSpy).toHaveBeenCalledWith(dto)
+  })
+
+  it('should return 400 if no championship is provided', async () => {
+    const { sut } = makeSut()
+
+    const dto = {
+      name: 'valid_name',
+      color: 'valid_color'
+    }
+
+    const result = await sut.create({ body: dto })
+
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toStrictEqual(new MissingParamError('championship'))
+  })
+
+  it('should return 400 if no name is provided', async () => {
+    const { sut } = makeSut()
+
+    const dto = {
+      championship: 'valid_championship',
+      color: 'valid_color'
+    }
+
+    const result = await sut.create({ body: dto })
+
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toStrictEqual(new MissingParamError('name'))
   })
 })
