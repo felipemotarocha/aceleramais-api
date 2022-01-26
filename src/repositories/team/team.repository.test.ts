494583795265e5ec1089687bc7391ced7eb673ef
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-25 23:11:06 -0300

    test: ensure TeamRepository can get all Teams by Championship

--- src/repositories/team/team.repository.test.ts
+++ src/repositories/team/team.repository.test.ts
@@ -2,18 +2,18 @@ import { Types } from 'mongoose'
 
 import { env } from '../../config/env.config'
 import { CreateTeamDto } from '../../dtos/team.dtos'
-// import Team from '../../entities/team.entity'
+import Team from '../../entities/team.entity'
 import MongooseHelper from '../../helpers/mongoose.helpers'
 import TeamModel from '../../models/team.model'
 import { MongoTeamRepository } from './team.repository'
 
 describe('Mongo Team Repository', () => {
-  //   const validTeam: Team = {
-  //     id: new Types.ObjectId() as any,
-  //     championship: new Types.ObjectId() as any,
-  //     name: 'Mercedes',
-  //     color: '#fff'
-  //   }
+  const validTeam: Team = {
+    id: new Types.ObjectId() as any,
+    championship: new Types.ObjectId() as any,
+    name: 'Mercedes',
+    color: '#fff'
+  }
 
   beforeAll(async () => {
     await MongooseHelper.connect(env.mongodbUrl)
@@ -60,5 +60,18 @@ describe('Mongo Team Repository', () => {
     await sut.create(dto)
 
     expect(createCollectionSpy).toHaveBeenCalledWith(dto)
+  })
+
+  it('should get all Teams by Championship', async () => {
+    const sut = makeSut()
+
+    await TeamModel.create(validTeam)
+
+    const result = await sut.getAll(validTeam.championship)
+
+    expect(result[0].id).toBeTruthy()
+    expect(result[0].championship).toStrictEqual(validTeam.championship)
+    expect(result[0].name).toBe(validTeam.name)
+    expect(result[0].color).toBe(validTeam.color)
   })
 })
