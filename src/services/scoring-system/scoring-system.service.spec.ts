5b493103070d9270c8577c54127e5ece1cda1eae
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-26 14:34:27 -0300

    test: ensure ScoringSystemService can create  a ScoringSystem

---
+++ src/services/scoring-system/scoring-system.service.spec.ts
@@ -0,0 +1,95 @@
+import ScoringSystem from '../../entities/scoring-system.entity'
+import { ScoringSystemRepositoryAbstract } from '../../repositories/scoring-system/scoring-system.repository'
+import {
+  ScoringSystemServiceAbstract,
+  ScoringSystemService
+} from './scoring-system.service'
+
+describe('Scoring System Service', () => {
+  const validScoringSystem = {
+    id: 'valid_id',
+    championship: 'valid_championship',
+    scoringSystem: { 1: 25, 2: 20 }
+  }
+
+  interface SutTypes {
+    scoringSystemRepositoryStub: ScoringSystemRepositoryAbstract
+    sut: ScoringSystemServiceAbstract
+  }
+
+  const makeSut = (): SutTypes => {
+    class ScoringSystemRepositoryStub
+    implements ScoringSystemRepositoryAbstract {
+      async create(): Promise<ScoringSystem> {
+        return validScoringSystem
+      }
+
+      async getOne(): Promise<ScoringSystem> {
+        return validScoringSystem
+      }
+
+      async update(): Promise<ScoringSystem> {
+        return validScoringSystem
+      }
+
+      async delete(): Promise<ScoringSystem> {
+        return validScoringSystem
+      }
+    }
+
+    const scoringSystemRepositoryStub = new ScoringSystemRepositoryStub()
+    const sut = new ScoringSystemService(scoringSystemRepositoryStub)
+
+    return { scoringSystemRepositoryStub, sut }
+  }
+
+  it('should create a Scoring System', async () => {
+    const { sut } = makeSut()
+
+    const dto = {
+      championship: 'valid_championship',
+      scoringSystem: { 1: 25, 2: 20 }
+    }
+
+    const result = await sut.create(dto)
+
+    expect(result).toStrictEqual(validScoringSystem)
+  })
+
+  it('should call ScoringSystemRepository create method with correct values', async () => {
+    const { sut, scoringSystemRepositoryStub } = makeSut()
+
+    const createScoringSystemSpy = jest.spyOn(
+      scoringSystemRepositoryStub,
+      'create'
+    )
+
+    const dto = {
+      championship: 'valid_championship',
+      scoringSystem: { 1: 25, 2: 20 }
+    }
+
+    await sut.create(dto)
+
+    expect(createScoringSystemSpy).toHaveBeenCalledWith(dto)
+  })
+
+  it('should throws if ScoringSystemRepository create method throws', async () => {
+    const { sut, scoringSystemRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(scoringSystemRepositoryStub, 'create')
+      .mockReturnValueOnce(
+        new Promise((_resolve, reject) => reject(new Error()))
+      )
+
+    const dto = {
+      championship: 'valid_championship',
+      scoringSystem: { 1: 25, 2: 20 }
+    }
+
+    const promise = sut.create(dto)
+
+    expect(promise).rejects.toThrow()
+  })
+})
