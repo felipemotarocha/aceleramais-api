7d6632090bdaa627cde3e5b731fb89f7bfae912d
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-26 14:47:37 -0300

    feat: ensure ScoringSystemController returns 201 on creation success

---
+++ src/controllers/scoring-system/scoring-system.controller.ts
@@ -0,0 +1,42 @@
+import { created } from '../../helpers/controllers.helpers'
+import {
+  HttpRequest,
+  HttpResponse
+} from '../../protocols/controllers.protocols'
+import { ScoringSystemServiceAbstract } from '../../services/scoring-system/scoring-system.service'
+
+export interface ScoringSystemControllerAbstract {
+  create(httpRequest: HttpRequest): Promise<HttpResponse>
+  getAll(httpRequest: HttpRequest): Promise<HttpResponse>
+  update(httpRequest: HttpRequest): Promise<HttpResponse>
+  delete(httpRequest: HttpRequest): Promise<HttpResponse>
+}
+
+export class ScoringSystemController
+implements ScoringSystemControllerAbstract {
+  private readonly scoringSystemService: ScoringSystemServiceAbstract
+
+  constructor(scoringSystemService: ScoringSystemServiceAbstract) {
+    this.scoringSystemService = scoringSystemService
+  }
+
+  async create(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const scoringSystem = await this.scoringSystemService.create(
+      httpRequest.body
+    )
+
+    return created(scoringSystem)
+  }
+
+  getAll(httpRequest: HttpRequest): Promise<HttpResponse> {
+    throw new Error('Method not implemented.')
+  }
+
+  update(httpRequest: HttpRequest): Promise<HttpResponse> {
+    throw new Error('Method not implemented.')
+  }
+
+  delete(httpRequest: HttpRequest): Promise<HttpResponse> {
+    throw new Error('Method not implemented.')
+  }
+}
