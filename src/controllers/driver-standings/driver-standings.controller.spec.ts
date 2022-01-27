91f047200a931cdb910f3f60b1a11a050a184557
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-27 15:19:25 -0300

    refactor: remove useless methods from DriverStandingsController

--- src/controllers/driver-standings/driver-standings.controller.spec.ts
+++ src/controllers/driver-standings/driver-standings.controller.spec.ts
@@ -1,4 +1,3 @@
-import { CreateDriverStandingsDto } from '../../dtos/driver-standings.dto'
 import DriverStandings from '../../entities/driver-standings'
 import { MissingParamError, ServerError } from '../../errors/controllers.errors'
 import { DriverStandingsServiceAbstract } from '../../services/driver-standings/driver-standings.service'
@@ -50,149 +49,15 @@ describe('Driver Standings Controller', () => {
     return { sut, driverStandingsServiceStub }
   }
 
-  it('should return 201 on creation success', async () => {
+  it('should return 200 on getting a Driver Standings by Championship', async () => {
     const { sut } = makeSut()
 
-    const dto: CreateDriverStandingsDto = {
-      championship: 'valid_championship',
-      standings: [
-        {
-          user: 'valid_user',
-          isRegistered: true,
-          position: 1
-        }
-      ]
-    }
-
-    const result = await sut.create({
-      body: dto
+    const result = await sut.getOne({
+      query: { championship: 'valid_championship_id' }
     })
 
-    expect(result.statusCode).toBe(201)
+    expect(result.statusCode).toBe(200)
     expect(result.body).toStrictEqual(validDriverStandings)
-  })
-
-  it('should call DriverStandingsService create method with correct values', async () => {
-    const { sut, driverStandingsServiceStub } = makeSut()
-
-    const createDriverStandingsSpy = jest.spyOn(
-      driverStandingsServiceStub,
-      'create'
-    )
-
-    const dto: CreateDriverStandingsDto = {
-      championship: 'valid_championship',
-      standings: [
-        {
-          user: 'valid_user',
-          isRegistered: true,
-          position: 1
-        }
-      ]
-    }
-
-    await sut.create({ body: dto })
-
-    expect(createDriverStandingsSpy).toHaveBeenCalledWith(dto)
-  })
-
-  it('should return 400 if no championship is provided', async () => {
-    const { sut } = makeSut()
-
-    const dto = {
-      standings: [
-        {
-          user: 'valid_user',
-          isRegistered: true,
-          position: 1
-        }
-      ]
-    }
-
-    const result = await sut.create({ body: dto })
-
-    expect(result.statusCode).toBe(400)
-    expect(result.body).toStrictEqual(new MissingParamError('championship'))
-  })
-
-  it('should return 400 if no standings is provided', async () => {
-    const { sut } = makeSut()
-
-    const dto = {
-      championship: 'valid_championship'
-    }
-
-    const result = await sut.create({ body: dto })
-
-    expect(result.statusCode).toBe(400)
-    expect(result.body).toStrictEqual(new MissingParamError('standings'))
-  })
-
-  it('should return 400 if user is null and isRegistered is true', async () => {
-    const { sut } = makeSut()
-
-    const dto = {
-      championship: 'valid_championship',
-      standings: [
-        {
-          userName: 'Max Verstappen',
-          isRegistered: true,
-          position: 1
-        }
-      ]
-    }
-
-    const result = await sut.create({ body: dto })
-
-    expect(result.statusCode).toBe(400)
-    expect(result.body).toStrictEqual(
-      new Error('Some user provided on the standings is invalid.')
-    )
-  })
-
-  it('should return 400 if user and userName are provided', async () => {
-    const { sut } = makeSut()
-
-    const dto = {
-      championship: 'valid_championship',
-      standings: [
-        {
-          user: 'valid_user',
-          userName: 'Max Verstappen',
-          isRegistered: false,
-          position: 1
-        }
-      ]
-    }
-
-    const result = await sut.create({ body: dto })
-
-    expect(result.statusCode).toBe(400)
-    expect(result.body).toStrictEqual(
-      new Error('Some user provided on the standings is invalid.')
-    )
-  })
-
-  it('should return 400 if user is provided and isRegistered is false', async () => {
-    const { sut } = makeSut()
-
-    const dto = {
-      championship: 'valid_championship',
-      standings: [
-        {
-          user: 'valid_user',
-          isRegistered: false,
-          position: 1
-        }
-      ]
-    }
-
-    const result = await sut.create({ body: dto })
-
-    expect(result.statusCode).toBe(400)
-    expect(result.body).toStrictEqual(
-      new Error('Some user provided on the standings is invalid.')
-    )
   })
 
   it('should call DriverStandingsService getOne method with correct values', async () => {
--- src/controllers/driver-standings/driver-standings.controller.ts
+++ src/controllers/driver-standings/driver-standings.controller.ts
@@ -1,10 +1,5 @@
 import { MissingParamError } from '../../errors/controllers.errors'
-import {
-  badRequest,
-  created,
-  ok,
-  serverError
-} from '../../helpers/controllers.helpers'
+import { badRequest, ok, serverError } from '../../helpers/controllers.helpers'
 import {
   HttpRequest,
   HttpResponse
@@ -12,8 +7,6 @@ import {
 import { DriverStandingsServiceAbstract } from '../../services/driver-standings/driver-standings.service'
 
 export interface DriverStandingsControllerAbstract {
-  create(httpRequest: HttpRequest): Promise<HttpResponse>
-  update(httpRequest: HttpRequest): Promise<HttpResponse>
   getOne(httpRequest: HttpRequest): Promise<HttpResponse>
 }
 
@@ -23,39 +16,6 @@ implements DriverStandingsControllerAbstract {
 
   constructor(driverStandingsService: DriverStandingsServiceAbstract) {
     this.driverStandingsService = driverStandingsService
-  }
-
-  async create(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['championship', 'standings']
-
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
-      }
-    }
-
-    const someUserIsInvalid = httpRequest.body.standings.some(
-      (item) =>
-        (!item.user && item.isRegistered) ||
-        (item.user && item.userName) ||
-        (item.user && !item.isRegistered)
-    )
-
-    if (someUserIsInvalid) {
-      return badRequest(
-        new Error('Some user provided on the standings is invalid.')
-      )
-    }
-
-    const driverStandings = await this.driverStandingsService.create(
-      httpRequest.body
-    )
-
-    return created(driverStandings)
-  }
-
-  update(httpRequest: HttpRequest): Promise<HttpResponse> {
-    throw new Error('Method not implemented.')
   }
 
   async getOne(httpRequest: HttpRequest): Promise<HttpResponse> {
