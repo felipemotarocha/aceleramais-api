aa9a409764dbed91b58fcb3135d707fd12de7e4b
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-27 15:58:19 -0300

    feat: add TeamStandingsController

---
+++ src/controllers/team-standings/team-standings.controller.ts
@@ -0,0 +1,40 @@
+import { MissingParamError } from '../../errors/controllers.errors'
+import { badRequest, ok, serverError } from '../../helpers/controllers.helpers'
+import {
+  HttpRequest,
+  HttpResponse
+} from '../../protocols/controllers.protocols'
+import { TeamStandingsServiceAbstract } from '../../services/team-standings/team-standings.service'
+
+export interface TeamStandingsControllerAbstract {
+  getOne(httpRequest: HttpRequest): Promise<HttpResponse>
+}
+
+export class TeamStandingsController
+implements TeamStandingsControllerAbstract {
+  private readonly teamStandingsService: TeamStandingsServiceAbstract
+
+  constructor(teamStandingsService: TeamStandingsServiceAbstract) {
+    this.teamStandingsService = teamStandingsService
+  }
+
+  async getOne(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      if (!httpRequest.query) {
+        return badRequest(new MissingParamError('query'))
+      }
+
+      if (!httpRequest.query?.championship) {
+        return badRequest(new MissingParamError('championship'))
+      }
+
+      const scoringSystem = await this.teamStandingsService.getOne({
+        championship: httpRequest.query.championship
+      })
+
+      return ok(scoringSystem)
+    } catch (_) {
+      return serverError()
+    }
+  }
+}
