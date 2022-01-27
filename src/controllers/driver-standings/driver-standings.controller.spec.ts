38381d6dbbfa8adcc3f185f55ff9dbf41db307fd
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-27 14:51:44 -0300

    test: ensure DriverStandingsController returns 200 when getting one by championship

--- src/controllers/driver-standings/driver-standings.controller.spec.ts
+++ src/controllers/driver-standings/driver-standings.controller.spec.ts
@@ -1,6 +1,6 @@
 import { CreateDriverStandingsDto } from '../../dtos/driver-standings.dto'
 import DriverStandings from '../../entities/driver-standings'
-import { MissingParamError } from '../../errors/controllers.errors'
+import { MissingParamError, ServerError } from '../../errors/controllers.errors'
 import { DriverStandingsServiceAbstract } from '../../services/driver-standings/driver-standings.service'
 import {
   DriverStandingsControllerAbstract,
@@ -193,5 +193,55 @@ describe('Driver Standings Controller', () => {
     expect(result.body).toStrictEqual(
       new Error('Some user provided on the standings is invalid.')
     )
+  })
+
+  it('should call DriverStandingsService getOne method with correct values', async () => {
+    const { sut, driverStandingsServiceStub } = makeSut()
+
+    const getOneDriverStandingssSpy = jest.spyOn(
+      driverStandingsServiceStub,
+      'getOne'
+    )
+
+    await sut.getOne({ query: { championship: 'valid_championship_id' } })
+
+    expect(getOneDriverStandingssSpy).toHaveBeenCalledWith({
+      championship: 'valid_championship_id'
+    })
+  })
+
+  it('should return 400 when getting a DriverStandingss without providing a query', async () => {
+    const { sut } = makeSut()
+
+    const result = await sut.getOne({ query: undefined })
+
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toStrictEqual(new MissingParamError('query'))
+  })
+
+  it('should return 400 when getting a DriverStandingss without providing a Championship', async () => {
+    const { sut } = makeSut()
+
+    const result = await sut.getOne({ query: { championship: null as any } })
+
+    expect(result.statusCode).toBe(400)
+    expect(result.body).toStrictEqual(new MissingParamError('championship'))
+  })
+
+  it('should return 500 if DriverStandingsService getOne method throws', async () => {
+    const { sut, driverStandingsServiceStub } = makeSut()
+
+    jest
+      .spyOn(driverStandingsServiceStub, 'getOne')
+      .mockReturnValueOnce(
+        new Promise((_resolve, reject) => reject(new Error()))
+      )
+
+    const result = await sut.getOne({
+      query: { championship: 'valid_championship_id' }
+    })
+
+    expect(result.statusCode).toBe(500)
+    expect(result.body).toStrictEqual(new ServerError())
   })
 })
