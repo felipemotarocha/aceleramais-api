4d41e34194b2e3b52ea8f3e90298eaf776755f54
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-27 23:41:50 -0300

    test: ensure ChampionshipRepository can get a Championship by ID

--- src/repositories/championship/championship.repository.test.ts
+++ src/repositories/championship/championship.repository.test.ts
@@ -59,7 +59,7 @@ describe('Mongo Driver Standings Repository', () => {
     expect(result.scoringSystem).toStrictEqual(dto.scoringSystem)
   })
 
-  it('should call ChampionshipModel with correct values', async () => {
+  it('should call ChampionshipModel create method with correct values', async () => {
     const sut = makeSut()
 
     const createChampionshipSpy = jest.spyOn(ChampionshipModel, 'create')
@@ -78,5 +78,59 @@ describe('Mongo Driver Standings Repository', () => {
     await sut.create(dto)
 
     expect(createChampionshipSpy).toHaveBeenCalledWith(dto)
+  })
+
+  it('should get a Championship by ID', async () => {
+    const sut = makeSut()
+
+    const dto = {
+      id: new Types.ObjectId(),
+      description: 'valid_description',
+      name: 'valid_name',
+      platform: 'valid_platform',
+      avatarImageUrl: 'valid_url',
+      races: [new Types.ObjectId() as any],
+      teams: [new Types.ObjectId() as any],
+      drivers: [{ user: new Types.ObjectId() as any, isRegistered: true }],
+      scoringSystem: new Types.ObjectId() as any
+    }
+
+    await ChampionshipModel.create({ _id: dto.id, ...dto })
+
+    const result = await sut.getOne({ id: dto.id as any })
+
+    expect(result.id).toBeTruthy()
+    expect(result.description).toStrictEqual(dto.description)
+    expect(result.name).toStrictEqual(dto.name)
+    expect(result.platform).toStrictEqual(dto.platform)
+    expect(result.avatarImageUrl).toStrictEqual(dto.avatarImageUrl)
+    expect(result.races).toStrictEqual(dto.races)
+    expect(result.teams).toStrictEqual(dto.teams)
+    expect(result.drivers).toStrictEqual(dto.drivers)
+    expect(result.scoringSystem).toStrictEqual(dto.scoringSystem)
+  })
+
+  it('should call ChampionshipModel findById method with correct values', async () => {
+    const sut = makeSut()
+
+    const getOneChampionshipSpy = jest.spyOn(ChampionshipModel, 'findById')
+
+    const dto = {
+      id: new Types.ObjectId(),
+      description: 'valid_description',
+      name: 'valid_name',
+      platform: 'valid_platform',
+      avatarImageUrl: 'valid_url',
+      races: [new Types.ObjectId() as any],
+      teams: [new Types.ObjectId() as any],
+      drivers: [{ user: new Types.ObjectId() as any, isRegistered: true }],
+      scoringSystem: new Types.ObjectId() as any
+    }
+
+    await ChampionshipModel.create({ _id: dto.id, ...dto })
+
+    await sut.getOne({ id: dto.id as any })
+
+    expect(getOneChampionshipSpy).toHaveBeenCalledWith(dto.id)
   })
 })
