023d2d3172cf2bdb4746d5c8a8fd511637d43ae2
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-28 22:54:43 -0300

    feat: ensure ChampionshipController returns 500 if service's create method throws

--- src/controllers/championship/championship.controller.ts
+++ src/controllers/championship/championship.controller.ts
@@ -2,7 +2,11 @@ import {
   InvalidFieldError,
   MissingParamError
 } from '../../errors/controllers.errors'
-import { badRequest, created } from '../../helpers/controllers.helpers'
+import {
+  badRequest,
+  created,
+  serverError
+} from '../../helpers/controllers.helpers'
 import {
   HttpRequest,
   HttpResponse
@@ -22,72 +26,76 @@ export class ChampionshipController implements ChampionshipControllerAbstract {
   }
 
   async create(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = [
-      'name',
-      'description',
-      'platform',
-      'races',
-      'scoringSystem'
-    ]
+    try {
+      const requiredFields = [
+        'name',
+        'description',
+        'platform',
+        'races',
+        'scoringSystem'
+      ]
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    const someRaceIsInvalid = httpRequest.body.races.some(
-      (race) => !race.track || !race.startDate
-    )
-
-    if (someRaceIsInvalid) {
-      return badRequest(new InvalidFieldError('races'))
-    }
-
-    const someScoringSystemIsInvalid = Object.keys(
-      httpRequest.body.scoringSystem
-    ).some((key) => {
-      return (
-        ![...Array(50).keys()].includes(parseInt(key)) ||
-        typeof httpRequest.body.scoringSystem[key] !== 'number'
-      )
-    })
-
-    if (someScoringSystemIsInvalid) {
-      return badRequest(new InvalidFieldError('scoringSystem'))
-    }
-
-    if (httpRequest.body.teams) {
-      const someTeamIsInvalid = httpRequest.body.teams.some(
-        (team) => !team.name
+      const someRaceIsInvalid = httpRequest.body.races.some(
+        (race) => !race.track || !race.startDate
       )
 
-      if (someTeamIsInvalid) {
-        return badRequest(new InvalidFieldError('teams'))
+      if (someRaceIsInvalid) {
+        return badRequest(new InvalidFieldError('races'))
       }
-    }
 
-    if (httpRequest.body.drivers) {
-      const someDriverIsInvalid = httpRequest.body.drivers.some(
-        (item) =>
-          (!item.user && item.isRegistered) ||
-          (item.user && item.userName) ||
-          (item.user && !item.isRegistered) ||
-          (item.userName && item.isRegistered) ||
-          item.isRegistered === undefined ||
-          item.isRegistered === null
-      )
+      const someScoringSystemIsInvalid = Object.keys(
+        httpRequest.body.scoringSystem
+      ).some((key) => {
+        return (
+          ![...Array(50).keys()].includes(parseInt(key)) ||
+          typeof httpRequest.body.scoringSystem[key] !== 'number'
+        )
+      })
 
-      if (someDriverIsInvalid) {
-        return badRequest(new InvalidFieldError('drivers'))
+      if (someScoringSystemIsInvalid) {
+        return badRequest(new InvalidFieldError('scoringSystem'))
       }
+
+      if (httpRequest.body.teams) {
+        const someTeamIsInvalid = httpRequest.body.teams.some(
+          (team) => !team.name
+        )
+
+        if (someTeamIsInvalid) {
+          return badRequest(new InvalidFieldError('teams'))
+        }
+      }
+
+      if (httpRequest.body.drivers) {
+        const someDriverIsInvalid = httpRequest.body.drivers.some(
+          (item) =>
+            (!item.user && item.isRegistered) ||
+            (item.user && item.userName) ||
+            (item.user && !item.isRegistered) ||
+            (item.userName && item.isRegistered) ||
+            item.isRegistered === undefined ||
+            item.isRegistered === null
+        )
+
+        if (someDriverIsInvalid) {
+          return badRequest(new InvalidFieldError('drivers'))
+        }
+      }
+
+      const championship = await this.championshipService.create({
+        createChampionshipDto: httpRequest.body
+      })
+
+      return created(championship)
+    } catch (_) {
+      return serverError()
     }
-
-    const championship = await this.championshipService.create({
-      createChampionshipDto: httpRequest.body
-    })
-
-    return created(championship)
   }
 
   getOne(httpRequest: HttpRequest): Promise<HttpResponse> {
