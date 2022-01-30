19a0b7b48a8f4bace3ef3d7f3b97628984dc1b27
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-30 17:06:08 -0300

    feat: add userName to UserModel

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,27 +1,34 @@
 import { model, Schema } from 'mongoose'
 
-const userSchema = new Schema({
-  _id: {
-    type: String,
-    required: true
+const userSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      required: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    userName: {
+      type: String,
+      required: true
+    },
+    provider: {
+      type: String,
+      required: true
+    }
   },
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  provider: {
-    type: String,
-    required: true
-  }
-})
+  { _id: false }
+)
 
 const UserModel = model('User', userSchema)
 
