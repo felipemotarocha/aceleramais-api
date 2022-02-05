ff611fc43186e65195bb85933726d5f646c080e7
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-04 23:19:19 -0300

    test: ensure Track Route returns 200 on getting all Tracks

---
+++ src/routes/track/track.routes.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+
+import app from '../../config/app.config'
+import { env } from '../../config/env.config'
+import MongooseHelper from '../../helpers/mongoose.helpers'
+import TrackModel from '../../models/championship.model'
+
+describe('Track Routes', () => {
+  beforeAll(async () => {
+    await MongooseHelper.connect(env.mongodbUrl)
+  })
+
+  beforeEach(async () => {
+    await TrackModel.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongooseHelper.disconnect()
+  })
+
+  it('should return all Tracks on success', async () => {
+    await request(app).get('/api/track').expect(200)
+  })
+})
