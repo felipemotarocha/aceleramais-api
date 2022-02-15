f963aceccbecc755db91ced60099a6ad6d68493b
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-15 15:55:47 -0300

    test: add Championship Route to get Championships

--- src/routes/championship/championship.routes.test.ts
+++ src/routes/championship/championship.routes.test.ts
@@ -63,4 +63,29 @@ describe('Championship Routes', () => {
 
     await request(app).get(`/api/championship/${body.id}`).expect(200)
   })
+
+  it('should get Championships by Driver', async () => {
+    const data = {
+      description: 'valid_description',
+      name: 'valid_name',
+      platform: 'valid_platform',
+      avatarImageUrl: 'valid_url',
+      races: [{ startDate: 'valid_start_date', track: new Types.ObjectId() }],
+      teams: [{ name: 'valid_name', color: 'valid_color' }],
+      drivers: [{ user: new Types.ObjectId(), isRegistered: true }],
+      bonifications: [{ name: 'valid_bonifcation', points: 1 }],
+      penalties: [{ name: 'valid_penalty', points: 1 }],
+      scoringSystem: { 1: 25, 2: 20 },
+      admins: [{ user: new Types.ObjectId(), isCreator: true }]
+    }
+
+    await request(app)
+      .post('/api/championship')
+      .set('Content-type', 'multipart/form-data')
+      .field('data', JSON.stringify(data))
+
+    await request(app)
+      .get(`/api/championship?driver=${data.drivers[0].user}`)
+      .expect(200)
+  })
 })
