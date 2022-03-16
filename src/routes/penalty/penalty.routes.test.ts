183542a660f68820ed9be9534df932faa514c7b3
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-16 18:13:56 -0300

    test: add Penalty Routes

---
+++ src/routes/penalty/penalty.routes.test.ts
@@ -0,0 +1,35 @@
+import { Types } from 'mongoose'
+import request from 'supertest'
+
+import app from '../../config/app.config'
+import { env } from '../../config/env.config'
+import MongooseHelper from '../../helpers/mongoose.helpers'
+import PenaltyModel from '../../models/penalty.model'
+
+describe('Penalty Routes', () => {
+  const validPenalty = {
+    name: 'valid_penalty',
+    points: 1,
+    championship: new Types.ObjectId()
+  }
+
+  beforeAll(async () => {
+    await MongooseHelper.connect(env.mongodbUrl)
+  })
+
+  beforeEach(async () => {
+    await PenaltyModel.deleteMany({})
+
+    await PenaltyModel.create(validPenalty)
+  })
+
+  afterAll(async () => {
+    await MongooseHelper.disconnect()
+  })
+
+  it('should get all Penalties by Championship', async () => {
+    await request(app)
+      .get(`/api/penalty?championship=${validPenalty.championship}`)
+      .expect(200)
+  })
+})
