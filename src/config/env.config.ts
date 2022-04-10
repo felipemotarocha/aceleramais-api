c8e62f1336a47111946f49f9c28af13d7bf39674
Felipe Rocha <felipemota.rocha@gmail.com>
2022-04-10 19:29:26 -0300

    fix: mongodb local connection string

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,5 +1,6 @@
 export const env = {
   mongodbUrl:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/sim-racer-api',
+    process.env.MONGO_URL ||
+    'mongodb://root:password@localhost:27017/sim-racer?authSource=admin',
   port: process.env.PORT || 5050
 }
