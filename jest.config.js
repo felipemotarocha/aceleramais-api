dc0deab0566527506b2a19f4b0d2403296d18525
Felipe Rocha <felipemota.rocha@gmail.com>
2022-05-07 15:30:26 -0300

    chore: add unit and integration tests scripts

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/dtos/*.ts',
+    '!<rootDir>/src/**/entities/*.ts',
+    '!<rootDir>/src/**/factories/*.ts',
+    '!<rootDir>/src/**/models/*.ts',
+    '!<rootDir>/src/**/protocols/*.ts',
+    '!<rootDir>/src/**/config/*.ts',
+    '!<rootDir>/src/**/errors/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  watchPathIgnorePatterns: ['globalConfig', '<rootDir>/dist/'],
+  testPathIgnorePatterns: ['<rootDir>/dist/']
+}
