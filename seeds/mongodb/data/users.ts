7724c2f6baa9f7ee0956ccbdff9802e74f200899
Felipe Rocha <felipemota.rocha@gmail.com>
2022-05-13 20:10:36 -0300

    feat: update user's seed file with biography

--- seeds/mongodb/data/users.ts
+++ seeds/mongodb/data/users.ts
@@ -7,7 +7,9 @@ const users = [
     userName: 'felipe.rocha',
     profileImageUrl:
       'https://lh3.googleusercontent.com/a-/AOh14Gg-QreTOcdpYlm9Jys5-NnkxK1gXhoxm-X53FLwcQ=s96-c',
-    provider: 'google'
+    provider: 'google',
+    biography:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mollis volutpat enim dapibus cras ornare. Volutpat nulla hendrerit convallis elit ut libero odio volutpat nulla. Neque, diam sed enim, id odio id purus. Risus nunc blandit tempus enim leo.'
   }
 ]
 
