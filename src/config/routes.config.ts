74cf6973515392a0df4e1ebd5d5a73c72aa95ec5
Felipe Rocha <felipemota.rocha@gmail.com>
2022-06-01 19:25:40 -0300

    refactor: route config import

--- src/config/routes.config.ts
+++ src/config/routes.config.ts
@@ -1,14 +1,31 @@
-import fg from 'fast-glob'
 import { Express, Router } from 'express'
+
+import bonificationRouter from '../routes/bonification/bonification.routes'
+import championshipRouter from '../routes/championship/championship.routes'
+import driverStandingsRouter from '../routes/driver-standings/driver-standings.routes'
+import penaltyRouter from '../routes/penalty/penalty.routes'
+import raceRouter from '../routes/race/race.routes'
+import raceClassificationRouter from '../routes/race-classification/race-classification.routes'
+import teamRouter from '../routes/team/team.routes'
+import teamStandingsRouter from '../routes/team-standings/team-standings.routes'
+import trackRouter from '../routes/track/track.routes'
+import userRouter from '../routes/user/user.routes'
 
 const setupRoutes = (app: Express): void => {
   const router = Router()
 
   app.use('/api', router)
 
-  fg.sync('**/src/routes/**/**.routes.ts').map(async (file) =>
-    (await import(`../../${file}`)).default(router)
-  )
+  bonificationRouter(router)
+  championshipRouter(router)
+  driverStandingsRouter(router)
+  penaltyRouter(router)
+  raceRouter(router)
+  raceClassificationRouter(router)
+  teamRouter(router)
+  teamStandingsRouter(router)
+  trackRouter(router)
+  userRouter(router)
 }
 
 export default setupRoutes
